32033102959fba4f2578b79a6f1926381e23005a
basdiniz <brunodiniz1818@gmail.com>
2022-06-13 20:04:57 -0300

    Basic laravel authentication

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 use App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
@@ -14,15 +15,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Auth::routes();
+
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes(['register' => true]);
 
 Route::resource("users",App\Http\Controllers\UserController::class,['except'=>['create','store']]);
 Route::resource("eventos",App\Http\Controllers\EventosController::class);
 Route::resource("noticias",App\Http\Controllers\NoticiasController::class);
 Route::resource("projetos",App\Http\Controllers\ProjetosController::class,['except'=>['create','store']]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
